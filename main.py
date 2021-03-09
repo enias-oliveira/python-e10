deef38dbc8e62846828f8c423fd1884d544b90fc
Enias Oliveira <eniasoliveira27@gmail.com>
2021-03-08 21:15:57 -0300

    fix: reimplements create, delete and update functions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import os.path
 
 
 def convert_dict_number_values_to_int(dict_):
@@ -20,32 +21,55 @@ def create_character(filename, name, intelligence, power, strength, agility):
         "strength",
         "agility",
     ]
-    with open(filename, "r") as readable_file:
-        reader = csv.DictReader(readable_file)
 
-        has_reader = reader.fieldnames == expected_field_names
+    if os.path.isfile(filename):
 
-        rows = list(reader)
-        total_rows = len(rows)
+        with open(filename, "r") as readable_file:
+            reader = csv.DictReader(readable_file)
 
-        new_character_id = total_rows + 1
+            has_reader = reader.fieldnames == expected_field_names
 
-        new_character = {
-            "id": new_character_id,
-            "name": name,
-            "intelligence": intelligence,
-            "power": power,
-            "strength": strength,
-            "agility": agility,
-        }
+            rows = list(reader)
 
-    with open(filename, "a") as writable_file:
-        writer = csv.DictWriter(writable_file, fieldnames=expected_field_names)
+            try:
+                last_id = rows[-1]["id"]
+            except IndexError:
+                last_id = 0
 
-        if not has_reader:
+            new_character_id = int(last_id) + 1
+
+            new_character = {
+                "id": new_character_id,
+                "name": name,
+                "intelligence": intelligence,
+                "power": power,
+                "strength": strength,
+                "agility": agility,
+            }
+
+        with open(filename, "a+") as writable_file:
+            writer = csv.DictWriter(writable_file, fieldnames=expected_field_names)
+
+            if not has_reader:
+                writer.writeheader()
+
+            writer.writerow(new_character)
+
+    else:
+        with open(filename, "w+") as writable_file:
+            writer = csv.DictWriter(writable_file, fieldnames=expected_field_names)
+
+            new_character = {
+                "id": 1,
+                "name": name,
+                "intelligence": intelligence,
+                "power": power,
+                "strength": strength,
+                "agility": agility,
+            }
+
             writer.writeheader()
-
-        writer.writerow(new_character)
+            writer.writerow(new_character)
 
     return new_character
 
@@ -92,8 +116,8 @@ def delete_character(filename, character_id):
 
         writer.writeheader()
 
-        for row in new_file:
-            writer.writerows(row)
+        for character in new_file:
+            writer.writerow(character)
 
     return True
 
@@ -103,15 +127,39 @@ def update_character(filename, character_id, **kwargs):
     try:
         character_to_update = find_character_by_id(filename, character_id)
         character_to_update.update(kwargs)
-        delete_character(filename, character_id)
     except ValueError:
         return None
 
-    with open(filename, "a+", newline="") as writable_file:
+    with open(filename, "r") as readable_file:
+
+        reader = csv.DictReader(readable_file)
+
+        new_file = [
+            character if character["id"] != str(character_id) else character_to_update
+            for character in reader
+        ]
+
+    with open(filename, "w") as writable_file:
 
         fieldnames = ["id", "name", "intelligence", "power", "strength", "agility"]
         writer = csv.DictWriter(writable_file, fieldnames=fieldnames)
 
-        writer.writerow(character_to_update)
+        writer.writeheader()
+
+        for character in new_file:
+            writer.writerow(character)
 
         return character_to_update
+
+
+if __name__ == "__main__":
+    csv_filename = "characters.csv"
+
+    create_character(csv_filename, "Hulk", 10, 10, 10, 10)
+    create_character(csv_filename, "Batman", 10, 10, 10, 10)
+
+    delete_character(csv_filename, 2)
+
+    create_character(csv_filename, "Spiderman", 10, 10, 10, 10)
+
+    update_character(csv_filename, 1, agility=1)
